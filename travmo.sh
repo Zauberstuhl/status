cc50523d9bd035a336288f45db46e7332aec581b
Lukas Matt <lukas@zauberstuhl.de>
2016-12-31 01:02:39 +0100

    Use max runtime for travis builds

--- travmo.sh
+++ travmo.sh
@@ -16,13 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# commit results to
+# working branch
 branch="monitoring"
-
-starting_time=$(date +%s)
-# the build should take at least 5 minutes
-# otherwise we trigger the rebuild too often
-min_duration=$(( 5 * 60 ))
+# https://docs.travis-ci.com/user/customizing-the-build#Build-Timeouts
+max_duration=$((45 * 60))
 
 function log {
   echo -ne "\n$@"
@@ -32,55 +29,60 @@ function log {
 head -n 11 README.md
 
 error=()
+unsuccessful=0
+successful=0
+first_run=true
 tests=$(find test/ -name '*.test' | sort)
 tests_cnt=$(echo $tests |grep -o ' ' |wc -l)
 # add one since last file has no blank
 ((tests_cnt+=1))
 
-for test in $tests; do
-  { # try executing test script if test
-    # starts with 01-09 it will be sourced
-    # adding the ability to create new
-    # runtime environment variables
-    basename=$(basename $test)
-    if [ ${basename:0:2} -lt 10 ]; then
-      output=$(source $test 2>&1)
-    else
-      output=$(bash $test 2>&1)
-    fi
-    log "[+] $test"
-  } || {
-    # add to error messages if execution failed
-    error+=("$test: $output") && log "[-] $test"
-  }
+starting_time=$(date +%s)
+ending_time=$(($starting_time + $max_duration))
+log "Found $tests_cnt tests (+ succeeded, - failed, x skipped)"
+
+while [[ $(date +%s) -lt $ending_time ]];
+do
+  for test in $tests; do
+    { # try executing test script if test
+      # starts with 01-09 it will be sourced
+      # adding the ability to create new
+      # runtime environment variables
+      basename=$(basename $test)
+      if [ ${basename:0:2} -lt 10 ]; then
+        if $first_run; then
+          first_run=false
+          output=$(source $test 2>&1)
+          log "[+] $test"
+        else
+          log "[x] $test"
+        fi
+      else
+        output=$(bash $test 2>&1)
+        log "[+] $test"
+      fi
+    } || {
+      # add to error messages if execution failed
+      error+=("$test: $output") && log "[-] $test"
+    }
+  done
+  unsuccessful=$(($unsuccessful + ${#error[@]}))
+  successful=$(($successful + ($tests_cnt - $unsuccessful)))
+  log "[ ] Successful: $successful Failed: $unsuccessful ($(date))"
+  sleep 10
 done
 
-ending_time=$(date +%s)
-diff_time=$(($ending_time - $starting_time))
-
-if [ $diff_time -lt $min_duration ]; then
-  sleep_time=$(($min_duration - $diff_time))
-  log "Sleeping for $sleep_time more seconds\n" && \
-    sleep $sleep_time
-fi
-
-unsuccessful=${#error[@]}
-successful=$(($tests_cnt - $unsuccessful))
-
-log "Tests: $tests_cnt Successful: $successful Failed: $unsuccessful\n"
-
-if [[ "$unsuccessful" != "0" ]]; then
-  # switch from detach mode
-  git branch -D $branch || log "No master branch found!\n"
-  git checkout -b $branch
-  # commit results with all error messages
-  git commit --allow-empty \
-    -m "Tests: $tests_cnt Successful: $successful Failed: $unsuccessful
+# switch from detach mode
+git branch -D $branch || log "No $branch branch found!\n"
+git checkout -b $branch
+# commit results with all error messages
+git commit --allow-empty \
+  -m "Tests: $tests_cnt Successful: $successful Failed: $unsuccessful
 
 $error"
-  # merge from upstream first
-  git pull origin $branch
-  # push to upstream
-  git push origin $branch && \
-    log "$(git log -1)\n"
-fi
+
+# merge from upstream first
+git pull origin $branch
+# push to upstream
+git push origin $branch && \
+  log "$(git log -1)\n"
